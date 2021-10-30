420abf151087a57524ba03d8d3c88bcb32254e91
Dan Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2021-10-30 14:07:39 -0500

    Add alts to icons

--- src/profile/contact-social.js
+++ src/profile/contact-social.js
@@ -8,39 +8,45 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons/faEnvelope";
 const ContactSocial = ({ capEmail, facebook, twitter }) => {
   const CONTACT_BUTTONS = [
     {
-      label: "Email",
+      id: "Email",
       value: capEmail,
       linkFormatter: (value) => `mailto:${value}`,
       icon: faEnvelope,
+      alt: "envelope icon"
     },
     {
-      label: "Facebook",
+      id: "Facebook",
       value: facebook,
       linkFormatter: (value) => `https://www.facebook.com/${value}`,
       icon: faFacebook,
+      alt: "Facebook logo"
+
     },
     {
-      label: "Twitter",
+      id: "Twitter",
       value: twitter,
       linkFormatter: (value) => `https://twitter.com/${value}`,
       icon: faTwitter,
+      alt: "Twitter logo"
+
     },
   ];
   return (
     <div className="w-full mb-6 flex gap-x-6 justify-center items-center ">
-      {CONTACT_BUTTONS.map(({ label, value, icon, linkFormatter }) => {
+      {CONTACT_BUTTONS.map(({ id, value, icon, alt, linkFormatter }) => {
         if (!value) {
           return null;
         }
         const cleanUrl = linkFormatter(value);
         return (
           <a
-            key={label}
+            key={id}
             href={cleanUrl}
             target="_blank"
             rel="noopener noreferrer"
           >
             <FontAwesomeIcon
+            alt={alt}
               className="text-xl text-center text-green-trib hover:text-opacity-60 transition-all duration-300"
               icon={icon}
             />
